1d0fa4f880ef95afada116058fba3b11d17b5306
Jason Lutz <jason.m.lutz@gmail.com>
2020-06-19 10:55:52 -0700

    Implement methods for building and populating grid
    Early functionallity includes the following:
    
     - create a grid of a specified size (width = height = size)
     - populate the grid with tiles (bombs or empty)
     - count bombs
     - simple random for whether a bomb is placed at tile
    	generation and how likely a bomb is to be placed

--- board.rb
+++ board.rb
@@ -1,10 +1,33 @@
+require_relative "tile"
+
 class Board
+  attr_reader :grid
+
   def initialize(size = 9)
     @size = size
     @grid = generate_grid
   end
 
   def generate_grid
-    
+    Array.new (@size) {
+      Array.new(@size) {nil}
+    }
   end
+
+  def random_seed(size = 3)
+    (0...size).to_a.sample
+  end
+
+  def populate_grid
+    @grid.each do |row|
+      row.map! do |el|
+        random_seed == 0 ? Tile.new(:bomb) : Tile.new(:empty)
+      end
+    end
+  end
+
+  def count_bombs
+    @grid.flatten.count { |el| el.value == :bomb }
+  end
+
 end
