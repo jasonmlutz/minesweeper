6ad4461386fcb86d3995fbea5032168aaa70822b
Jason Lutz <jason.m.lutz@gmail.com>
2020-06-19 13:48:09 -0700

    Add method Board#count_bombs and dependencies...
    These methods are untested at this time; the intended flow is as follows:
    	1. loop through each tile in the board
    	2. fetch the tiles adjacent to tile T
    	3. count the bombs present in those tiles
    	4. update T.value to be this count, unless T is a bomb
    
    This will be tested using Board#cheat and Board.render.

--- board.rb
+++ board.rb
@@ -31,6 +31,36 @@ class Board
     @grid[row][col]
   end
 
+  def grid_intersect(arr_of_indices)
+    # arr is expected to be a one-dimensional array
+    # which could represent a subset of the row/column
+    # indices of @grid
+    # 
+    # this method returns only those indices
+    # that are within the range to be indices for
+    # a row/column of @grid
+    arr_of_indices.intersection((0...@size).to_a)
+  end
+
+  def grid_select(arr_of_coords)
+    # returns the tiles of @grid which are at the given coords
+    tiles = []
+    arr_of_coords.each do |coords|
+      row, col = coords
+      tiles << self[row, col]
+    end
+    tiles
+  end
+
+  def adj_bombs(row, col)
+    adj_rows = grid_intersect((row-1..row+1).to_a)
+    adj_cols = grid_intersect((col-1..col+1).to_a)
+    adj_coords = adj_rows.product(adj_cols).delete([row, col])
+
+    tiles = grid_select(adj_coords)
+    tiles.count { |tile| tile.value == :bomb}
+  end
+
   def count_bombs
     @grid.flatten.count { |el| el.value == :bomb }
   end
