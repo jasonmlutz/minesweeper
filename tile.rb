62b3f0a831c7fc065d35711f4da68420d76861b3
Jason Lutz <jason.m.lutz@gmail.com>
2020-06-20 07:32:42 -0700

    Update attr accessor & reader for Tile class ...
    ... so that Tile methods follow more natural verbage; change
    to be reflected in Board#reveal_all and Board#hide_all.

--- tile.rb
+++ tile.rb
@@ -1,8 +1,18 @@
 class Tile
-  attr_accessor :value, :revealed
+  attr_accessor :value
+  attr_reader :revealed
 
   def initialize(value)
     @value = value
     @revealed = false
   end
+
+  def reveal
+    @revealed = true
+  end
+
+  def hide
+    @revealed = false
+  end
+  
 end
