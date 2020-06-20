2ca7d02180296bc0e8c9cecb805d085865394050
Jason Lutz <jason.m.lutz@gmail.com>
2020-06-20 10:04:09 -0700

    Prepare for initla Game test

--- game.rb
+++ game.rb
@@ -1,5 +1,24 @@
+require_relative "board"
+
 class Game
-  def initialize
-    
+  def initialize(size = 10, difficulty = 1)
+    @board = Board.new(size, difficulty)
   end
+
+  def play_turn
+    board.render
+    puts "enter a position to reveal e.g. 0,4"
+    pos = (gets.chomp).split(",")
+    success = @board.reveal(pos)
+  end
+
+  def play_game
+    until won? || lost?
+      play_turn
+    end
+    board.render
+    puts "you win!" if won?
+    puts "you lose!" if lost?
+  end
+  
 end
